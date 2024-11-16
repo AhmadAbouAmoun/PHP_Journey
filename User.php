4d7e466fc4903f3c4d01d1580d3e98970602279c
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-16 17:35:15 +0200

    created the validate password method

--- User.php
+++ User.php
@@ -7,4 +7,17 @@ class user{
         $this->email=$email;
         $this->password=$password;
     }
+    public function check_password (){
+        $regex='/^(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{12,}$/';
+        if(preg_match($regex,$this->password)){
+            echo"right password format ";
+            return true;
+        }
+        else{
+            echo"wrong password format ";
+            return false;
+        }
+    }
 } 
+$user=new user("ahmad","AaAa#awfqewgeg");
+$user->check_password();
