87219951f1889fbd3b115724441d500032dd7ce8
Ahmad Abou Amoun <aabouamoun@gmail.com>
2024-11-16 16:37:33 +0200

    added the logic to print the double voweled nodes

--- PrintingVowels.php
+++ PrintingVowels.php
@@ -1,4 +1,8 @@
 <?php
+function check_2_vowels($word){
+    preg_match_all("/[aeoiuAEIOU]/",$word,$matches);
+    return count($matches[0])>=2;
+}
 class node{
     public $data;
     public $next;
@@ -27,5 +31,24 @@ class LinkedList{
             $current->next=$node;
         }
     }
-    
+    public function display_2_Voweled_nodes(){
+        if($this->head===null){
+            echo"list is empty";
+            return;
+        }
+        $current=$this->head;
+        while($current->next!=null){
+            if(check_2_vowels($current->data)){
+            echo"$current->data";
+            $current=$current->next;
+            }
+        
+        }
+    }
 }
+$list = new LinkedList();
+$list->appendLL("hello");
+$list->appendLL("sky");
+$list->appendLL("area");
+$list->appendLL("yes");
+$list->display_2_Voweled_nodes();
